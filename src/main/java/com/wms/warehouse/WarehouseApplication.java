54ead017c8ac587bbb64b5e929c7dbde685545a1
ATAnhTuan <anhtuan.itwork@gmail.com>
2024-01-10 08:57:31 +0700

    first commit

---
+++ src/main/java/com/wms/warehouse/WarehouseApplication.java
@@ -0,0 +1,14 @@
+package com.wms.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApplication.class, args);
+    }
+
+}
