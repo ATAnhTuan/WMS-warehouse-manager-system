a59d813469fe93328ab4ef82cd45fcb04bfd163a
longnn26 <anhtuan.itwork@gmail.com>
2024-01-31 15:58:32 +0700

    Revert "feat(task):add task with time"
    This reverts commit 0a675110c252c48acc29068a8c61854e0318c74e.

--- src/main/java/com/wms/warehouse/controller/RoleController.java
+++ src/main/java/com/wms/warehouse/controller/RoleController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("api/v1")
 @Tag(name = "RoleController")
 public class RoleController {
     final private RoleService roleService;
